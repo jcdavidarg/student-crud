19e783eb69cc7d3fe04209276cfb14ada785ff8e
jcdavidarg <jcdavidarg@gmail.com>
2026-08-31 10:31:36 +0000

    testeando cgi

--- cgi-bin/students.pl
+++ cgi-bin/students.pl
@@ -2,9 +2,17 @@
 
 use strict;
 use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use DB;
+use StudentRepository;
+use StudentService;
 use JSON::PP;
+use CGI;
 
-
+=pod
 my $students = [
     {
         id       => 1,
@@ -17,7 +25,28 @@ my $students = [
         apellido => 'Gomez'
     }
 ];
+=cut
+
+=pod
+my $dbh = DB::connect();
+
+my $repository = StudentRepository->new($dbh);
+my $service    = StudentService->new($repository);
+
+my $students = $service->get_students();
 
 print "Content-Type: application/json\n\n";
 
 print encode_json($students);
+
+$dbh->disconnect();
+=cut
+
+=pod
+my $cgi = CGI->new;
+
+my $method = $cgi->request_method;
+
+print "Content-Type: text/plain\n\n";
+print "Metodo HTTP: $method\n";
+=cut
